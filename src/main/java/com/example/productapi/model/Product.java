267b4d97899486d830b52208fa850d993845b729
Aktar <apaktar@gmail.com>
2024-11-13 18:21:04 +0600

    Initial commit

---
+++ src/main/java/com/example/productapi/model/Product.java
@@ -0,0 +1,43 @@
+package com.example.productapi.model;
+import jakarta.persistence.*;
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
